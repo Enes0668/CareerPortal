26e21cf053e24d2d875c1f1fa73066e70724c286
Enes0668 <maxigaming949@gmail.com>
2025-09-27 13:01:33 +0300

    Postgresql changes2

--- KariyerPortali/Program.cs
+++ KariyerPortali/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using KariyerPortali.Models;
 using System;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,8 @@ builder.Services.AddControllersWithViews();
 
 // DbContext ekle
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 // Email gönderimi için IEmailSender implementasyonu
 builder.Services.AddTransient<IEmailSender, EmailSender>();
